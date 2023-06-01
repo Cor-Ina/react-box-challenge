c88a2dd0c7dfb68ba5d0371f1349e7597965f2d8
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-06-01 09:06:19 -0400

    Update toggle function

--- src/App.js
+++ src/App.js
@@ -9,24 +9,37 @@ export default function App(props) {
 
   const [squares, setSquares] = React.useState(boxes)
 
-  
   function toggle(id) {
-    setSquares(prevSquares => {
-      const newSquares =[]
-      for (let i = 0; i < prevSquares.length; i++) {
-        if (prevSquares[i].id === id) {
-          const updatedSquare = {
-            ...prevSquares[i],
-            on: !prevSquares[i].on
-          }
-          newSquares.push(updatedSquare)
-        } else {
-          newSquares.push(prevSquares[i])
-        }
-      }
-      return newSquares
-    })
+    setSquares(prevSquares => prevSquares.map(square => square.id === id ? {...square, on: !square.on} : square))
   }
+ 
+
+  // function toggle(id) {
+  //   setSquares(prevSquares => {
+  //       return prevSquares.map((square) => {
+  //           return square.id === id ? {...square, on: !square.on} : square
+  //       })
+  //   })
+  // }
+
+  
+  // function toggle(id) {
+  //   setSquares(prevSquares => {
+  //     const newSquares =[]
+  //     for (let i = 0; i < prevSquares.length; i++) {
+  //       if (prevSquares[i].id === id) {
+  //         const updatedSquare = {
+  //           ...prevSquares[i],
+  //           on: !prevSquares[i].on
+  //         }
+  //         newSquares.push(updatedSquare)
+  //       } else {
+  //         newSquares.push(prevSquares[i])
+  //       }
+  //     }
+  //     return newSquares
+  //   })
+  // }
 
 
   const squareElements = squares.map(square => (
